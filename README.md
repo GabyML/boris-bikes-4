0d242eedeefbf9273c1df627ffb3e53ba8bbd4af
GabyML <gabriela.lagunes.11@uclac.uk>
2015-07-23 14:25:41 +0100

    Create full? and empty? methods

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Boris Bikes excersice, second try. File and code created on July the 22nd, 2015. Work done by Owen and Gaby.
+Boris Bikes excersice, third try. File and code created on July the 23nd, 2015. Work done by Richard and Gaby.
--- lib/DockingStation_Class.rb
+++ lib/DockingStation_Class.rb
@@ -1,18 +1,29 @@
 require_relative 'Bike_Class'
 
 class DockingStation #definition of DockingStation class
+  
   def initialize
     @bikes = []
   end
 
 	def release_bike #definition of release_bike method
-		fail 'No bikes available' if @bikes.empty? #Guard Condition (presence of bikes)
+		fail 'No bikes available' if empty? #Guard Condition (presence of bikes)
 		@bikes.pop # All Ruby instance variables are initially nil by default
 	end
 
 	def dock (bike)
-		fail 'Docking Station full' if @bikes.count >= 20
+		fail 'Docking Station full' if full?
 		@bikes << bike
+	end
+
+	private
+
+	def full?
+		@bikes.count >= 20
+	end
+
+	def empty?
+		@bikes.empty?
 	end
 
 
